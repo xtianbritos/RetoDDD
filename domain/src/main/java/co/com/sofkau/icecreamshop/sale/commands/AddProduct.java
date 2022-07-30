1bc16031899c382e6ec258684a23b79c47b00344
Xtian <xtiancuenta1@gmail.com>
2022-07-30 15:14:09 -0300

    Se corrigió el comando AddProduct

--- domain/src/main/java/co/com/sofkau/icecreamshop/sale/commands/AddProduct.java
+++ domain/src/main/java/co/com/sofkau/icecreamshop/sale/commands/AddProduct.java
@@ -1,6 +1,38 @@
 package co.com.sofkau.icecreamshop.sale.commands;
 
 import co.com.sofka.domain.generic.Command;
+import co.com.sofkau.icecreamshop.sale.values.Amount;
+import co.com.sofkau.icecreamshop.sale.values.Name;
+import co.com.sofkau.icecreamshop.sale.values.Price;
+import co.com.sofkau.icecreamshop.sale.values.ProductId;
 
 public class AddProduct extends Command {
+    private final ProductId productId;
+    private final Name name;
+    private final Amount amount;
+    private final Price price;
+
+    public AddProduct(ProductId productId, Name name, Amount amount, Price price) {
+        this.productId = productId;
+        this.name = name;
+        this.amount = amount;
+        this.price = price;
+    }
+
+    //Getters
+    public ProductId getProductId() {
+        return productId;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public Amount getAmount() {
+        return amount;
+    }
+
+    public Price getPrice() {
+        return price;
+    }
 }
